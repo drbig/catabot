4d94db9dfd7aa6b71fd9f07b93b79250e7528a22
Piotr S. Staszewski <p.staszewski@gmail.com>
2015-04-11 21:01:38 +0200

    Messy hacking - WIP 6
    Added basic Seen plugin.

---
+++ plugins/seen.rb
@@ -0,0 +1,61 @@
+module CataBot
+  module Plugin
+    module Seen
+      VERSION = '0.0.2'
+
+      EXPIRE = CataBot.config['params']['jq']['expire']
+
+      class IRC
+        include Cinch::Plugin
+        set :prefix, /#{CataBot.config['irc']['nick']}.? /i
+
+        @@seen = Hash.new
+        @@mutex = Mutex.new
+
+        class LastSeen < Struct.new(:action, :where, :stamp)
+          def to_s
+            "#{action}ing channel #{where} at #{stamp.utc.strftime('%Y-%m-%d %H:%M:%S UTC')}"
+          end
+        end
+
+        listen_to :join, method: :join
+        def join(m)
+          @@seen[m.user.nick] = LastSeen.new(:join, m.channel, Time.now)
+        end
+
+        listen_to :part, method: :part
+        def part(m)
+          @@seen[m.user.nick] = LastSeen.new(:part, m.channel, Time.now)
+        end
+
+        CataBot::IRC.cmd('seen [nick]', 'Check last presence of nick')
+        match /seen (.*)$/, method: :seen
+        def seen(m, query)
+          @@mutex.synchronize do
+            if @@seen.has_key? query
+              m.reply "Last seen #{query} #{@@seen[query]}", true
+            else
+              m.reply "Don't recall seeing #{query}", true
+            end
+          end
+        end
+
+        CataBot.add_thread :seen_expire do
+          loop do
+            sleep(60 * 60)
+            CataBot.log :debug, 'Running Seen cleaner thread...'
+            threshold = Chronic.parse(EXPIRE)
+            deleted = 0
+            @@seen.each_pair do |k, v|
+              if v[:stamp] < threshold
+                @@mutex.synchronize { @@seen.delete(k) }
+                deleted += 1
+              end
+            end
+            CataBot.log :debug, "Seen cleaner: #{deleted} deleted, #{@@seen.length} kept"
+          end
+        end
+      end
+    end
+  end
+end
