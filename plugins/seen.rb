7eb94be9c40e2a697d709fe181febf1385bcfd19
Piotr S. Staszewski <p.staszewski@gmail.com>
2015-04-29 09:14:23 +0200

    Persistent and better Seen plugin
    Logs to database, should handle all possible events. Hopefully the
    cleaning thread is correct...

--- plugins/seen.rb
+++ plugins/seen.rb
@@ -14,41 +14,50 @@ module CataBot
           include DataMapper::Resource
 
           property :nick, String, key: true, length: 1..16
-
-          property :mask, String, required: true, length: 3..256
-          property :channel, String, required: true, length: 1..64
-          property :stamp, DateTime, required: true
           property :cmd, String, required: true, length: 1..32
+          property :stamp, DateTime, required: true
+          property :mask, String, length: 3..256
+          property :channel, String, length: 1..64
 
           def reply
             at = self.stamp.to_time.utc.strftime('%Y-%m-%d %H:%M:%S UTC')
-            "at #{at} (#{self.cmd} @ #{self.channel})"
+            where = self.channel ? " on #{self.channel}" : ''
+            "at #{at} (#{self.cmd}#{where})"
           end
         end
 
         def record(m, u = nil)
           user = u || m.user
           ns = NickSeen.first_or_create(nick: user.nick)
-          ns.mask = user.mask
-          ns.channel = m.channel.to_s
-          ns.stamp = DateTime.now
           ns.cmd = m.command.to_s.downcase
-          ns.save!
+          ns.stamp = DateTime.now
+
+          begin
+            mask = user.mask
+            ns.mask = mask
+          rescue StandardError
+            # just leave the mask as it was
+          end
+
+          begin
+            chan = m.channel.to_s
+            chan = nil if chan.empty?
+          rescue StandardError
+            chan = nil
+          ensure
+            ns.channel = chan
+          end
+
+          ns.save
         end
 
         listen_to :join, method: :join
-        def join(m)
-          CataBot.log :debug, "Seen #{m.user.nick} joining"
-          record(m)
-        end
+        def join(m); record(m); end
 
         listen_to :leaving, method: :leaving
-        def leaving(m, user)
-          CataBot.log :debug, "Seen #{m.user.nick} leaving (c: #{m.command}, e: #{m.events.join('; ')})"
-          record(m, user)
-        end
+        def leaving(m, user); record(m, user); end
 
-        command(:seen, /seen nick (.*)$/, 'seen nick [nick]', 'Check last known presence of [nick]')
+        command(:seen, /seen (.*)$/, 'seen [nick]', 'Check last known presence of [nick]')
         def seen(m, query)
           if ns = NickSeen.get(query)
             m.reply "Last seen #{query} #{ns.reply}", true
@@ -57,17 +66,20 @@ module CataBot
           end
         end
 
-        #CataBot.aux_thread(:seen_expire, 24 * 60 * 60) do
-        #  threshold = Chronic.parse(EXPIRE)
-        #  deleted = 0
-        #  @@seen.each_pair do |k, v|
-        #    if v[:stamp] < threshold
-        #      @@mutex.synchronize { @@seen.delete(k) }
-        #      deleted += 1
-        #    end
-        #  end
-        #  CataBot.log :debug, "Seen cleaner: #{deleted} deleted, #{@@seen.length} kept"
-        #end
+        CataBot.aux_thread(:seen_expire, 24 * 60 * 60) do
+          threshold = Chronic.parse(EXPIRE).to_datetime
+          expired = NickSeen.all(:stamp.lt => threshold)
+          deleted = 0
+
+          if expired.any?
+            deleted = expired.length
+            unless expired.destroy
+              CataBot.log :error, "Couldn't destroy expired last seen data: #{expired.errors.join(', ')}"
+            end
+          end
+
+          CataBot.log :debug, "Seen cleaner: #{deleted} deleted, #{NickSeen.count} kept"
+        end
       end
     end
   end
