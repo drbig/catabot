13b522e10100f6a36f005b5b71ca7ef71bf2be51
Piotr S. Staszewski <p.staszewski@gmail.com>
2015-06-06 19:08:54 +0200

    Add more features to Facts plugin
    About, delete by author, voting limiter, better messages.

--- plugins/facts.rb
+++ plugins/facts.rb
@@ -19,14 +19,19 @@ module CataBot
       class IRC
         include CataBot::IRC::Plugin
 
-        HELP = 'Can do: facts [keyword], fact add [keyword] [text], fact vote [up|down] [id], fact stats'
+        @@mutex = Mutex.new
+        @@voters = Hash.new
+        def self.reset_voters!
+          @@mutex.synchronize { @@voters.clear }
+        end
+
+        HELP = 'Can do: facts [keyword], fact add [keyword] [text], fact vote [up|down] [id], fact about [id], fact del [id], fact stats'
         command(:facts, /(facts?) ?(\w+)? ?(.*)?$/, 'fact', 'Ask about facts I know. See "fact help"')
         def facts(m, cmd, scmd, rest)
           unless m.channel?
             m.reply 'This only works in the context of a channel', true
             return
           end
-
           if cmd == 'facts'
             keyword = scmd.downcase
             facts = Fact.all(keyword: keyword, order: [:score.desc], channel: m.channel)
@@ -72,6 +77,11 @@ module CataBot
                 m.reply "Sorry, couldn't find fact (#{id})", true
                 return
               end
+              key = "#{m.user.mask}-#{id}"
+              if @@voters.has_key? key
+                m.reply "You've already voted for #{id} today, try tomorrow", true
+                return
+              end
               if dir == :up
                 fact.score += 1
               else
@@ -90,8 +100,43 @@ module CataBot
                   m.reply 'Erm, something went wrong. I\'ve logged the fact', true
                   return
                 end
-                m.reply "Fact (#{fact.id}) has now score of #{fact.score}"
+                m.user.msg "Fact (#{fact.id}) has now score of #{fact.score}"
               end
+              @@voters[key] = true
+            when 'del'
+              id = rest
+              unless id =~ /^\d+$/
+                m.reply 'Sorry, id must be a number', true
+                return
+              end
+              fact = Fact.get(id.to_i)
+              unless fact
+                m.reply "Sorry, couldn't find fact (#{id})", true
+                return
+              end
+              mask = Cinch::Mask.new(fact.user)
+              if mask.match(m.user.mask)
+                unless fact.destroy
+                  CataBot.log :error, "Facts: Error destroying by owner: #{fact}!"
+                  m.reply 'Erm, something went wrong. I\'ve logged the fact', true
+                  return
+                end
+                m.reply "Fact (#{fact.id}) removed by author"
+              else
+                m.user.msg "Sorry. You don't look like author of fact (#{fact.id})"
+              end
+            when 'about'
+              id = rest
+              unless id =~ /^\d+$/
+                m.reply 'Sorry, id must be a number', true
+                return
+              end
+              fact = Fact.get(id.to_i)
+              unless fact
+                m.reply "Sorry, couldn't find fact (#{id})", true
+                return
+              end
+              m.reply "(#{fact.id}) by #{Cinch::Mask.new(fact.user).nick} added on #{fact.stamp.utc.strftime('%Y-%m-%d %H:%M:%S UTC')}, score: #{fact.score}"
             when 'stats'
               all = Fact.all(channel: m.channel).count
               keywords = Fact.all(fields: [:keyword], unique: true, channel: m.channel).count
@@ -101,6 +146,8 @@ module CataBot
             end
           end
         end
+
+        CataBot.aux_thread(:facts_clean, 24 * 60 * 60) { IRC.reset_voters! }
       end
     end
   end
