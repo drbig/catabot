eaf5bd7855343b1fc04100c07d3c6f1d82fb2976
Piotr S. Staszewski <p.staszewski@gmail.com>
2016-06-22 21:09:20 +0200

    Unfrek the time in zone/place command
    Also thanks to #ruby for great tips while I was unfreking my Ruby env :)

--- plugins/clock.rb
+++ plugins/clock.rb
@@ -7,16 +7,24 @@ module CataBot
       class IRC
         include CataBot::IRC::Plugin
 
-        command(:time, /time (.*)$/, 'time [zone]', 'Show current time in [zone]')
-        def time(m, query)
-          query.gsub!(' ', '_')
+        TZ_MAP = TZInfo::Timezone.all.inject(Hash.new) {|acc, tz| acc[tz.name.downcase] = tz.name; acc }
+
+        command(:time_in, /time in (.*)$/, 'time in [where|zone]', 'Show current time in zone or country/place (case-sensitive!)')
+        def time_in(m, query)
+          query = clean_time_string(query)
           begin
             zone = TZInfo::Timezone.get(query)
           rescue TZInfo::InvalidTimezoneIdentifier
-            m.reply 'Couldn\'t find that zone. Maybe try US/Pacific or Europe/Warsaw', true
+            m.reply 'Couldn\'t find that zone. Maybe try US/Pacific or Europe/Warsaw or CET', true
           else
             m.reply "in #{zone.friendly_identifier} it's #{zone.now.strftime('%H:%M:%S (%Y-%m-%d)')}"
           end
+        end
+
+        private
+        def clean_time_string(string)
+          cleaned = string.gsub(' ', '_').downcase
+          TZ_MAP.fetch(cleaned, string)
         end
       end
     end
